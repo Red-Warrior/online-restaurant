05d48a6020dc660b6f30221b51f2964ebc9681b4
Red-Warrior <sword_and_brush@mail.ru>
2021-01-25 16:21:36 +0300

    completely

---
+++ components/carousel/index.js
@@ -0,0 +1,97 @@
+export default class Carousel {
+
+  constructor(slides) {
+    this.slides = slides;
+    this.elem = this.render(this.slides);
+    this.elem.addEventListener("click", (event) => this.onClick(event));
+
+    this.value = 0;
+    this.curentSlide = 1;
+
+    this.slideLenght = this.elem.querySelectorAll(".carousel__slide");
+    this.left = this.elem.querySelector(".carousel__arrow_left");
+    this.right = this.elem.querySelector(".carousel__arrow_right");
+
+    this.left.style.display = "none";
+
+    this.elem.addEventListener("click", (event) => this.slider(event));
+  }
+
+  slider(event) {
+    let counter = this.elem.querySelector(".carousel__inner").offsetWidth;
+
+    if (event.target.closest(".carousel__arrow_left")) {
+      this.elem.querySelector(".carousel__inner").style.transform = `translateX(${this.value += counter}px)`;
+      this.curentSlide -= 1;
+
+      if (this.curentSlide === 1) {
+        this.left.style.display = "none";
+      }
+      else if (this.curentSlide !== this.slideLenght.length) {
+        this.right.style.display = "";
+      } else {
+        this.left.style.display = "";
+      }
+    }
+
+    if (event.target.closest(".carousel__arrow_right")) {
+      this.elem.querySelector(".carousel__inner").style.transform = `translateX(${this.value -= counter}px)`;
+      this.curentSlide += 1;
+
+      if (this.curentSlide === this.slideLenght.length) {
+        this.right.style.display = "none";
+      } else if (this.curentSlide !== 1) {
+        this.left.style.display = "";
+      } else {
+        this.right.style.display = "";
+      }
+    }
+  }
+
+  onClick(event) {
+    if (event.target.closest(".carousel__button")) {
+      let customEvent = new CustomEvent("product-add", { bubbles: true, detail: this.slides[this.curentSlide - 1].id });
+      this.elem.dispatchEvent(customEvent);
+    }
+  }
+
+  render(slides) {
+
+    let carousel = document.createElement("div");
+    carousel.classList.add("carousel");
+    carousel.insertAdjacentHTML("afterbegin", `
+    <div class="carousel__arrow carousel__arrow_right">
+      <img src="/assets/images/icons/angle-icon.svg" alt="icon">
+    </div>
+    <div class="carousel__arrow carousel__arrow_left">
+      <img src="/assets/images/icons/angle-left-icon.svg" alt="icon">
+    </div>
+    `);
+
+    let divCarouselInner = document.createElement("div");
+    divCarouselInner.classList.add("carousel__inner");
+
+    for (const value of slides) {
+
+      let divCarouselSlide = document.createElement("div");
+      divCarouselSlide.classList.add("carousel__slide");
+      divCarouselSlide.setAttribute("data-id", `${value.id}`);
+
+      divCarouselSlide.insertAdjacentHTML("afterbegin", `
+      <img src="/assets/images/carousel/${value.image}" class="carousel__img" alt="slide">
+      <div class="carousel__caption">
+          <span class="carousel__price">€${value.price.toFixed(2)}</span>
+          <div class="carousel__title">${value.name}</div>
+          <button type="button" class="carousel__button">
+              <img src="/assets/images/icons/plus-icon.svg" alt="icon">
+          </button>
+      </div>
+      `);
+
+      divCarouselInner.append(divCarouselSlide);
+    }
+
+    carousel.append(divCarouselInner);
+    return carousel;
+  }
+}
---
+++ components/cart/index.js
@@ -0,0 +1,253 @@
+import createElement from '../../assets/lib/create-element.js';
+import escapeHtml from '../../assets/lib/escape-html.js';
+
+export default class Cart {
+  cartItems = []; // [product: {...}, count: N]
+
+  constructor(cartIcon) {
+    this.cartIcon = cartIcon;
+    this.addEventListeners();
+    this.targetId;
+  }
+
+  addProduct(product) {
+    let target = this.cartItems.findIndex(item => item.product.id === product.id);
+    if (target >= 0) {
+      this.cartItems[target].count += 1;
+    } else {
+      this.cartItems.push({ product: product, count: 1 })
+    }
+    this.onProductUpdate(this.cartItems);
+  }
+
+  updateProductCount(productId, amount) {
+    let targetIndex = this.cartItems.findIndex(item => item.product.id === productId);
+
+    if (this.cartItems[targetIndex].count > 0) {
+      this.cartItems.find(item => item.product.id === productId).count += (amount);
+    }
+    if (this.cartItems[targetIndex].count === 0) {
+      this.cartItems.splice(targetIndex, 1);
+      document.querySelector(`[data-product-id="${this.targetId}"]`).remove();
+    }
+
+    this.onProductUpdate(this.cartItems);
+  }
+
+  isEmpty() {
+    return this.cartItems.length === 0;
+  }
+
+  getTotalCount() {
+    let totalCount = 0
+    for (let elem of this.cartItems) {
+      totalCount += elem.count;
+    }
+    return totalCount;
+  }
+
+  getTotalPrice() {
+    let totalPrice = 0;
+    for (let elem of this.cartItems) {
+      totalPrice += elem.product.price * elem.count;
+    }
+    return totalPrice;
+  }
+
+  renderProduct(product, count) {
+    return createElement(`
+    <div class="cart-product" data-product-id="${product.id}">
+      <div class="cart-product__img">
+        <img src="/assets/images/products/${product.image}" alt="product">
+      </div>
+      <div class="cart-product__info">
+        <div class="cart-product__title">${escapeHtml(product.name)}</div>
+        <div class="cart-product__price-wrap">
+          <div class="cart-counter">
+            <button type="button" class="cart-counter__button cart-counter__button_minus">
+              <img src="/assets/images/icons/square-minus-icon.svg" alt="minus">
+            </button>
+            <span class="cart-counter__count">${count}</span>
+            <button type="button" class="cart-counter__button cart-counter__button_plus">
+              <img src="/assets/images/icons/square-plus-icon.svg" alt="plus">
+            </button>
+          </div>
+          <div class="cart-product__price">€${product.price.toFixed(2)}</div>
+        </div>
+      </div>
+    </div>`);
+  }
+
+  renderOrderForm() {
+    return createElement(`<form class="cart-form">
+      <h5 class="cart-form__title">Delivery</h5>
+      <div class="cart-form__group cart-form__group_row">
+        <input name="name" type="text" class="cart-form__input" placeholder="Name" required value="Santa Claus">
+        <input name="email" type="email" class="cart-form__input" placeholder="Email" required value="john@gmail.com">
+        <input name="tel" type="tel" class="cart-form__input" placeholder="Phone" required value="+1234567">
+      </div>
+      <div class="cart-form__group">
+        <input name="address" type="text" class="cart-form__input" placeholder="Address" required value="North, Lapland, Snow Home">
+      </div>
+      <div class="cart-buttons">
+        <div class="cart-buttons__buttons btn-group">
+          <div class="cart-buttons__info">
+            <span class="cart-buttons__info-text">total</span>
+            <span class="cart-buttons__info-price">€${this.getTotalPrice().toFixed(2)}</span>
+          </div>
+          <button type="submit" class="cart-buttons__button btn-group__button button">order</button>
+        </div>
+      </div>
+    </form>`);
+  }
+
+  renderModal() {
+    document.body.classList.add("is-modal-open");
+    let divModal = document.createElement("div");
+    divModal.classList.add("modal");
+    divModal.insertAdjacentHTML("afterbegin", `
+    <div class="modal__overlay"></div>
+    <div class="modal__inner">
+      <div class="modal__header">
+        <!--Кнопка закрытия модального окна-->
+        <button type="button" class="modal__close">
+          <img src="/assets/images/icons/cross-icon.svg" alt="close-icon" />
+        </button>
+        <h3 class="modal__title">Your order</h3>
+      </div>
+      <div class="modal__body">
+
+      </div>
+    </div>
+    `);
+
+    document.body.append(divModal);
+
+    for (let elem of this.cartItems) {
+      document.querySelector(".modal__body").append(this.renderProduct(elem.product, elem.count));
+    }
+
+    document.querySelector(".modal__body").append(this.renderOrderForm());
+    document.querySelector(".modal").addEventListener("click", (event) => counter(event));
+
+    let counter = (event) => {
+      if (event.target.closest(".cart-counter__button_minus")) {
+        this.targetId = event.target.closest(".cart-product").getAttribute("data-product-id");
+
+        for (let elem of this.cartItems) {
+          if (this.targetId === elem.product.id) {
+            this.updateProductCount(this.targetId, -1)
+            this.onProductUpdate(this.cartItems);
+          }
+        }
+      }
+
+      if (event.target.closest(".cart-counter__button_plus")) {
+        this.targetId = event.target.closest(".cart-product").getAttribute("data-product-id");
+
+        for (let elem of this.cartItems) {
+          if (this.targetId === elem.product.id) {
+            this.updateProductCount(this.targetId, 1)
+            this.onProductUpdate(this.cartItems);
+          }
+        }
+      }
+    }
+
+    if (document.querySelector(".cart-form")) {
+      document.querySelector(".cart-form").addEventListener("submit", (event) => {
+        event.preventDefault();
+        this.onSubmit();
+      });
+    }
+    document.querySelector(".modal__close").addEventListener("click", () => this.buttonClose());
+    document.addEventListener("keydown", (event) => this.keyHandler(event));
+  }
+
+  onProductUpdate(elem) {
+    if (document.querySelector(".is-modal-open")) {
+
+      for (let elem of this.cartItems) {
+        if (this.targetId === elem.product.id) {
+          document.querySelector(`[data-product-id="${this.targetId}"] .cart-counter__count`)
+            .textContent = elem.count;
+          document.querySelector(`[data-product-id="${this.targetId}"] .cart-product__price`)
+            .textContent = `€${(elem.product.price * elem.count).toFixed(2)}`;
+          document.querySelector(".cart-buttons__info-price").innerHTML = `€${this.getTotalPrice().toFixed(2)}`;
+        }
+      }
+    }
+
+    if (this.cartItems.length === 0) {
+      document.body.classList.remove("is-modal-open");
+      if (document.querySelector(".modal")) {
+        document.querySelector(".modal").remove();
+      }
+    }
+    this.cartIcon.update(this);
+  }
+
+  onSubmit() {
+    document.querySelector("[type=submit]").classList.add("is-loading");
+
+    let formElem = document.querySelector(".cart-form");
+    let userFormData = new FormData(formElem);
+
+    let result = fetch('https://httpbin.org/post', {
+      method: 'POST',
+      body: userFormData
+    });
+
+    result.then(() => {
+      document.querySelector(".modal__title").textContent = "Success!";
+      this.cartItems = [];
+
+      document.querySelector(".modal__body").innerHTML = `
+        <div class="modal__body-inner">
+          <p>
+             Order successful! Your order is being cooked :) <br>
+             We’ll notify you about delivery time shortly.<br>
+             <img src="/assets/images/delivery.gif">
+          </p>
+        </div>`;
+    });
+  };
+
+  addEventListeners() {
+    this.cartIcon.elem.onclick = () => this.renderModal();
+  }
+
+
+  close() {
+    document.body.classList.remove("is-modal-open");
+    if (document.querySelector(".modal")) {
+      document.querySelector(".modal").remove();
+    }
+
+  }
+
+  buttonClose() {
+    if (document.querySelector(".modal__body-inner")) {
+      this.cartIcon.update(this);
+    }
+
+
+    document.body.classList.remove("is-modal-open");
+    if (document.querySelector(".modal")) {
+      document.querySelector(".modal").remove();
+    }
+  }
+
+  keyHandler(event) {
+    if (document.querySelector(".modal__body-inner")) {
+      this.cartIcon.update(this);
+    }
+
+    if (event.code === "Escape") {
+      document.body.classList.remove("is-modal-open");
+      if (document.querySelector(".modal")) {
+        document.querySelector(".modal").remove();
+      }
+    }
+  }
+}
